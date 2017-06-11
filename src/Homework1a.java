aa1eccd9f624d9642fac496bedeb52cb992b8455
Viktor-Lenovo <dennis.veytsman@gmail.com>
2017-06-11 12:50:02 -0400

    added variable nothing

--- src/Homework1a.java
+++ src/Homework1a.java
@@ -1,29 +1,31 @@
 public class Homework1a {
 
+// let's push code to github
 
     public static void main(String[] args) {
-        int i,j,sum;
-        boolean b = true;
+        int i, j;
+        int nothing = 0;
+        boolean isOdd = true;
         for (i = 0; i < 10; i++) {
             for (j = 0; j < 5; j++) {
-                sum = i + j;
-                b = ((sum % 2) != 0);
+                int sum = i + j;
+                isOdd = ((sum % 2) != 0);
                 System.out.println("i = " + i);
                 System.out.println("j = " + j);
-                if (b) {
+                if (isOdd) {
                     System.out.println("sum = " + sum + " - odd");
                 } else {
                     System.out.println("sum = " + sum + " - even");
-                    }
+                }
             }
         }
     }
 }
-    // i = 0 .. 10     j = 0 .. 5
-    // sum = i + j
-    //even = 2, 4, 6 -? even?
-    //int modulus = 13 % 2; //1 - остаток
-       // modulus = 10 % 2; //0
+// i = 0 .. 10     j = 0 .. 5
+// sum = i + j
+//even = 2, 4, 6 -? even?
+//int modulus = 13 % 2; //1 - остаток
+// modulus = 10 % 2; //0
 //number is even if modulus == 0
 
 
